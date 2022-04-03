e75600494929e6449c6e9f84393face6724e23d3
Basheer <you@example.com>
2022-04-03 21:38:22 +0100

    time change

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,12 +215,12 @@
         mm = parseInt(mm) + 1;
         mm = String(mm).padStart(2, '0');
     }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
+    // var timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
     
 
     // Use this for real timer date
-    /*  var timerdate = "2020/01/01"; */
+   var timerdate = "2022/04/18"; 
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span><p>Days</p> </div>" + "<div class='cd-item'><span>%H</span><p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span><p>Min</p> </div>" + "<div class='cd-item'><span>%S</span><p>Sec</p> </div>"));
